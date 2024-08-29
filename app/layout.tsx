9aca23f5b07060b3a7a2e5cc5edb25ebddf5c349
Andcool-Systems <drolll2007@gmail.com>
2024-08-29 21:59:58 +0300

    changed design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const Roboto = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "AndcoolSystems",
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={Roboto.className}>{children}</body>
     </html>
   );
 }
