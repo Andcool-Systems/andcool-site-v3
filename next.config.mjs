d67f19fe044f98ab43afb3854ff61f3af94b0b0f
Andcool-Systems <drolll2007@gmail.com>
2024-11-17 21:57:00 +0300

    added mc-oauth site link

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
     reactStrictMode: false,
     experimental: {
         missingSuspenseWithCSRBailout: false
-    }
+    },
+    webpack(config) {
+        config.cache = false;
+        return config;
+    },
 };
 
 export default nextConfig;
